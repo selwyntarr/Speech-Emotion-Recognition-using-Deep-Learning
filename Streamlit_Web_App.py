69b5b5947b157c0246b7a7a603dfd9dab7c0d9f9
jmaducal12 <112488446+jmaducal12@users.noreply.github.com>
2023-05-27 17:36:53 +0800

    Update Streamlit_Web_App.py

--- Streamlit_Web_App.py
+++ Streamlit_Web_App.py
@@ -3,7 +3,6 @@ import tensorflow as tf
 import librosa
 import numpy as np
 import io
-from PIL import Image
 
 # Load the pre-trained model
 model = tf.keras.models.load_model('ltsm_best_weights1.hdf5')
@@ -12,16 +11,16 @@ model = tf.keras.models.load_model('ltsm_best_weights1.hdf5')
 emotion_labels = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad']
 
 # Function to extract audio features from the uploaded audio file
-def extract_features(audio_data, sample_rate):
-    # Resample audio to a fixed sample rate
-    y = librosa.resample(audio_data, sample_rate, 22050)
-    # Compute spectrogram
-    D = librosa.stft(y)
-    spect = librosa.amplitude_to_db(np.abs(D), ref=np.max)
-    # Resize spectrogram to match the expected input shape of the model
-    spect = tf.image.resize(spect, [162, 128])
-    spect = tf.expand_dims(spect, axis=-1)
-    return spect
+def extract_features(audio_data):
+    audio_bytes = io.BytesIO(audio_data)
+    y, sr = librosa.load(audio_bytes, sr=None)
+    mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40)
+    mfccs_scaled = np.mean(mfccs.T, axis=0)
+    # Reshape the features to match the expected input shape of the model
+    features = np.expand_dims(mfccs_scaled, axis=0)
+    features = np.expand_dims(features, axis=-1)
+    features = tf.image.resize(features, [162, 1])  # Reshape to (162, 1)
+    return features
 
 # Main function for creating the Streamlit app
 def main():
@@ -32,25 +31,17 @@ def main():
 
     if audio_file is not None:
         audio_data = audio_file.read()
-        audio_bytes = np.frombuffer(audio_data, dtype=np.uint8)
-        
-        try:
-            # Load audio data and get sample rate
-            audio, sample_rate = librosa.load(io.BytesIO(audio_bytes), sr=None, mono=True)
-            
-            st.audio(audio_data, format='audio/wav')
+        st.audio(audio_data, format='audio/wav')
 
-            if st.button('Recognize Emotion'):
-                try:
-                    features = extract_features(audio, sample_rate)
-                    features = tf.expand_dims(features, axis=0)
-                    predicted_probabilities = model.predict(features)[0]
-                    predicted_emotion = emotion_labels[np.argmax(predicted_probabilities)]
-                    st.success(f'Predicted Emotion: {predicted_emotion}')
-                except Exception as e:
-                    st.error(f'Error: {e}')
-        except Exception as e:
-            st.error(f'Error loading audio file: {e}')
+        if st.button('Recognize Emotion'):
+            try:
+                features = extract_features(audio_data)
+                features = np.expand_dims(features, axis=0)
+                predicted_probabilities = model.predict(features)[0]
+                predicted_emotion = emotion_labels[np.argmax(predicted_probabilities)]
+                st.success(f'Predicted Emotion: {predicted_emotion}')
+            except Exception as e:
+                st.error(f'Error: {e}')
 
 if __name__ == '__main__':
     main()
